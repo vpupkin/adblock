c30754a158d84036f8cfed97edc25a8a32354481
alter.ELV1S <alter.ELV1S@db2f4e91-8153-0410-91ae-15c7cea2a5af>
2009-11-02 22:38:25 +0000

    - trunk/adblock-debug.css

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,1 +1,1 @@
-sed -e 's/background-color:#f99 !important; color:#900 !important; outline:1px solid #f33 !important/display: none !important;/g' -e 's/ debug//g' adblock-debug.css > adblock.css
+sed -e 's/background-color:#f99 !important; color:#900 !important; outline:1px solid #f33 !important/display: none !important;/g' -e 's/ debug//g' ../branches/adblock-debug.css > adblock.css
